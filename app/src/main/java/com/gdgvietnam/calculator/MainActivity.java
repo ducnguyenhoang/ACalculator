ca9b5c606d76611eaaf6244954169be535147db3
Duc Nguyen <hoangduc007@hotmail.com>
2015-12-17 01:13:39 +0700

    no message

--- app/src/main/java/com/gdgvietnam/calculator/MainActivity.java
+++ app/src/main/java/com/gdgvietnam/calculator/MainActivity.java
@@ -9,43 +9,67 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnSetting;
+    Button btnBracketLeft;
+    Button btnBracketRight;
+    Button btnAllClear;
+    Button btnDelete;
+
+    Button btnN7;
+    Button btnN8;
+    Button btnN9;
+    Button btnN6;
+    Button btnN5;
+    Button btnN4;
+    Button btnN3;
+    Button btnN2;
+    Button btnN1;
+    Button btnN0;
+
+    Button btnDot;
+    Button btnAdd;
+    Button btnMinus;
+    Button btnSub;
+    Button btnMulti;
+
+    Button btnResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnSetting = (Button) findViewById(R.id.btn_setting);
-        Button btnBracketLeft = (Button) findViewById(R.id.btn_bracket_left);
-        Button btnBracketRight = (Button) findViewById(R.id.btn_bracket_right);
-        Button btnAllClear = (Button) findViewById(R.id.btn_all_clear);
-        Button btnDelete = (Button) findViewById(R.id.btn_delete);
+        //btnSetting = (Button) findViewById(R.id.btn_setting);
+        btnBracketLeft = (Button) findViewById(R.id.btn_bracket_left);
+        btnBracketRight = (Button) findViewById(R.id.btn_bracket_right);
+        btnAllClear = (Button) findViewById(R.id.btn_all_clear);
+        btnDelete = (Button) findViewById(R.id.btn_delete);
 
-        Button btnN7 = (Button) findViewById(R.id.btn_n7);
-        Button btnN8 = (Button) findViewById(R.id.btn_n8);
-        Button btnN9 = (Button) findViewById(R.id.btn_n9);
-        Button btnN6 = (Button) findViewById(R.id.btn_n6);
-        Button btnN5 = (Button) findViewById(R.id.btn_n5);
-        Button btnN4 = (Button) findViewById(R.id.btn_n4);
-        Button btnN3 = (Button) findViewById(R.id.btn_n3);
-        Button btnN2 = (Button) findViewById(R.id.btn_n2);
-        Button btnN1 = (Button) findViewById(R.id.btn_n1);
-        Button btnN0 = (Button) findViewById(R.id.btn_n0);
+        btnN7 = (Button) findViewById(R.id.btn_n7);
+        btnN8 = (Button) findViewById(R.id.btn_n8);
+        btnN9 = (Button) findViewById(R.id.btn_n9);
+        btnN6 = (Button) findViewById(R.id.btn_n6);
+        btnN5 = (Button) findViewById(R.id.btn_n5);
+        btnN4 = (Button) findViewById(R.id.btn_n4);
+        btnN3 = (Button) findViewById(R.id.btn_n3);
+        btnN2 = (Button) findViewById(R.id.btn_n2);
+        btnN1 = (Button) findViewById(R.id.btn_n1);
+        btnN0 = (Button) findViewById(R.id.btn_n0);
 
-        Button btnDot = (Button) findViewById(R.id.btn_dot);
-        Button btnAdd = (Button) findViewById(R.id.btn_add);
-        Button btnMinus = (Button) findViewById(R.id.btn_minus);
-        Button btnSub = (Button) findViewById(R.id.btn_sub);
-        Button btnMulti = (Button) findViewById(R.id.btn_multi);
+        btnDot = (Button) findViewById(R.id.btn_dot);
+        btnAdd = (Button) findViewById(R.id.btn_add);
+        btnMinus = (Button) findViewById(R.id.btn_minus);
+        btnSub = (Button) findViewById(R.id.btn_sub);
+        btnMulti = (Button) findViewById(R.id.btn_multi);
 
-        Button btnResult = (Button) findViewById(R.id.btn_result);
+        btnResult = (Button) findViewById(R.id.btn_result);
 
 
-        btnResult.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+        btnResult.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                TextView tvResult = (TextView) findViewById(R.id.tv_show_result);
-                tvResult.setText("Hello World");
+            public void onClick(View v) {
+                TextView result = (TextView) findViewById(R.id.tv_show_result);
+                result.setText("Hello world");
             }
         });
 
